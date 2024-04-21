f5b7ecc759a05f8cd8204ab30cdae79c2a3fa44a
Renato Neto <rnato.netoo@gmail.com>
2024-04-20 23:02:57 -0300

    project base

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,10 +1,13 @@
+import 'regenerator-runtime/runtime'
 import Phaser from 'phaser'
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import Bootstrap from './scenes/Bootstrap'
+import Game from './scenes/Game'
+import GameOver from './scenes/GameOver'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: 800,
+	width: 600,
 	height: 600,
 	physics: {
 		default: 'arcade',
@@ -12,7 +15,7 @@ const config: Phaser.Types.Core.GameConfig = {
 			gravity: { y: 200 }
 		}
 	},
-	scene: [HelloWorldScene]
+	scene: [Bootstrap, Game, GameOver]
 }
 
 export default new Phaser.Game(config)
